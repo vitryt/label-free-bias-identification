207d380c5a00fc7a6def8db3ee8dbacd982a1be9
Kieran Edgeworth <edgeworthkieran@gmail.com>
2025-12-07 16:47:15 +0100

    Expanding functionality of bias_identifying.py to utilise Waterbirds and different models

--- bias_identifying.py
+++ bias_identifying.py
@@ -5,9 +5,11 @@ import pickle as pkl
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import random
+import numpy as np
+# import wandb
+import logging
 
 import torch
-from src.colour_mnist import get_biased_mnist_dataloader
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
@@ -16,19 +18,20 @@ from craft.craft_torch import Craft, torch_to_numpy
 import src.model_utils as mu
 import src.concept_utils as cu
 
-# import wandb
+# Dataset imports
+from src.colour_mnist import get_biased_mnist_dataloader
+from src.waterbird import WaterBirdsDataset
 
-import numpy as np
+# Model imports
+from src.resnet18_utils import WaterbirdsResNet18
+from src.resnet50_utils import WaterbirdsResNet50
 
-import logging
 logging.getLogger('tensorflow').disabled = True
-
 sys.path.append(os.getcwd())
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_id", type=int, default=0)
 parser.add_argument("--model_name", type=str, default="MNIST")
-
 parser.add_argument("--concept_id", type=str, default="0")
 parser.add_argument("--layer_depth", type=int, default=4)
 parser.add_argument("--number_of_concept", type=int, default=40)
@@ -41,12 +44,10 @@ parser.add_argument("--result_path", type=str, default="")
 parser.add_argument("--data_path", type=str, default="")
 parser.add_argument("--multi_concept", type=bool, default=False)
 
-
 args = parser.parse_args()
 
 model_id = args.model_id
 model_name = args.model_name
-
 concept_id = args.concept_id
 layer_depth = args.layer_depth
 number_of_concept = args.number_of_concept
@@ -55,31 +56,124 @@ concept_dataset_size = args.concept_dataset_size
 # backprop_step = args.backprop_step
 backprop_steps = [1, 10, 30, 70, 100, 300, 700, 1000]
 
-data_path = args.data_path
-if data_path == "":
-    data_path = os.getcwd()
+# Configuring paths
+# data_path = args.data_path
+# if data_path == "":
+#     data_path = os.getcwd()
+# data_path += "/data/" + model_name
+data_path = args.data_path or os.getcwd()
 data_path += "/data/" + model_name
-
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
 
-result_path = args.result_path + "models/"
-result_path += model_name + "/"
+# result_path = args.result_path + "models/"
+# result_path += model_name + "/"
+# model_result_path = result_path + f"model_{model_id}.pkl"
 
-model_result_path = result_path + f"model_{model_id}.pkl"
-if os.path.exists(model_result_path):
-    with open(model_result_path, "rb") as f:
-        model_parameters = pkl.load(f)
-    batch_size = model_parameters["batch_size"]
-    train_correlation = model_parameters["train_correlation"]
-    test_correlation = model_parameters["test_correlation"]
-    split_seed = model_parameters["split_seed"]
-    shuffle_seed = model_parameters["shuffle_seed"]
+result_path = os.path.join(args.result_path, "models", model_name)
+model_result_path = os.path.join(result_path, f"model_{model_id}.pkl")
+
+if not os.path.exists(model_result_path):
+    raise FileExistsError(f"Error ! You need to train the model before being able to test it! Path: {model_result_path}")
+
+with open(model_result_path, "rb") as f:
+    model_parameters = pkl.load(f)
+batch_size = model_parameters["batch_size"]
+split_seed = model_parameters["split_seed"]
+shuffle_seed = model_parameters["shuffle_seed"]
+
+# Specifying dataset loader
+if model_name == "MNIST":
+    train_loader, val_loader = get_biased_mnist_dataloader(
+        root = data_path,
+        batch_size = batch_size,
+        data_label_correlation = model_parameters["train_correlation"],
+        train = True,
+        validation = 1/10,
+        split_gen_seed = split_seed,
+        shuffle_seed = shuffle_seed
+    )
 else:
-    print("Error ! You need to train the model before being able to test it !")
-    raise FileExistsError(model_result_path)
+    train_loader = WaterBirdsDataset(
+        root_dir = data_path,
+        split = "train"
+    )
+    val_loader = WaterBirdsDataset(
+        root_dir = data_path,
+        split = "val"
+    )
+    train_loader = DataLoader(
+        train_loader,
+        batch_size = batch_size,
+        shuffle = True
+    )
+    val_loader = DataLoader(
+        val_loader,
+        batch_size = batch_size,
+        shuffle = False
+    )
 
-result_path += f"concepts_{model_id}_{concept_id}.pkl"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+if model_name == "MNIST":
+    model = mu.CMNISTNeuralNetwork()
+elif model_name == "Waterbirds18":
+    model = WaterbirdsResNet18()
+elif model_name == "Waterbirds50":
+    model = WaterbirdsResNet50()
+else:
+    raise ValueError(f"Unknown model name: {model_name}")
+
+model = model.to(device)
+model.load_state_dict(torch.load(model_result_path))
+
+loss_fn = nn.CrossEntropyLoss()
+
+# Setting up the concept decomposition
+print(f"Using: {device}")
+print(f"-Training the concept decomposition", end="\r")
+
+concept_dataset = torch.Tensor([])
+for X, *_ in val_loader:
+    if len(concept_dataset) >= concept_dataset_size:
+        break
+    concept_dataset = torch.cat((concept_dataset, X))
+
+# CRAFT layer extraction
+if model_name == "MNIST":
+    g = nn.Sequential(*(list(model.children())[:layer_depth-1])) # Layers pre concept decomposition
+    h = nn.Sequential(*(list(model.children())[layer_depth-1:])) # Layers post concept decompositon
+else:
+    g = nn.Sequential(*(list(model.backbone.children())[:-1])) # Layers pre concept decomposition
+    h = nn.Sequential(nn.Flatten(), model.backbone.fc) # Layers post concept decompositon
+
+craft = Craft(
+    input_to_latent=g,
+    latent_to_logit = h,
+    number_of_concepts = number_of_concept,
+    patch_size = patch_size,
+    batch_size = batch_size,
+    device = device
+)
+
+crops, crops_u, w = craft.fit(concept_dataset)
+print("-Concept decomposition trained successfully")
+crops = np.moveaxis(torch_to_numpy(crops), 1, -1)
+gr = cu.Gradient_retriever(h)
+
+results = {}
+for backprop_step in backprop_steps:
+    print(f"-Gathering concepts with backprop step : {backprop_step}", end="\r")
+    results[backprop_step] = cu.gather_all_concept_results(
+        val_loader,
+        model,
+        loss_fn,
+        {"all": craft},
+        gr,
+        backprop_multi = backprop_step,
+        device = device
+    )
+    print(f"-Concepts with backprop step {backprop_step} gathered successfully")
+
 parameters = {
     "model_id":model_id,
     "concept_id":concept_id,
@@ -88,125 +182,17 @@ parameters = {
     "patch_size":patch_size,
     "concept_dataset_size":concept_dataset_size,
     "backprop_steps":backprop_steps,
-
+    "concept_results": results,
+    "concept_parameters": {
+        "W": w,
+        "crops": crops,
+        "crops_u": crops_u,
+        "reducer": craft.reducer,
+    },
 }
 
-if os.path.exists(result_path):
-    print(f"\n\n XXXXXXXX Concept experiment number {model_id}|{concept_id} skipped, already done XXXXXXXX")
-else :
-    print(f"XXXXXXXX Starting concept experiment {model_id}|{concept_id} with parameters :", parameters)
-    # run = wandb.init(
-    #     entity="thomas-vitry",
-    #     project="CVDB",
-    #     config=parameters,
-    # )
-
-    train_dataloader, validation_dataloader = get_biased_mnist_dataloader(root=data_path, batch_size=batch_size, data_label_correlation=train_correlation, train=True, validation=1/10, split_gen_seed=split_seed, shuffle_seed=shuffle_seed)
-    test_dataloader = get_biased_mnist_dataloader(root=data_path, batch_size=batch_size, data_label_correlation=test_correlation, train=False, shuffle_seed=shuffle_seed)
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f"Using {device} device")
-
-    model = (mu.CMNISTNeuralNetwork() if model_parameters["model_name"] == "MNIST" else None).to(device)
-    assert(os.path.exists(args.result_path + f"models/{model_name}/model_{model_id}"))
-    model.load_state_dict(torch.load(args.result_path + f"models/{model_name}/model_{model_id}"))
-
-    loss_fn = nn.CrossEntropyLoss()
-
-    if not args.multi_concept:
-        g = nn.Sequential(*(list(model.children())[:layer_depth-1])) # Layers pre concept decomposition
-        h = nn.Sequential(*(list(model.children())[layer_depth-1:])) # Layers post concept decompositon
-
-        craft = Craft(
-            input_to_latent=g,
-            latent_to_logit = h,
-            number_of_concepts = number_of_concept,
-            patch_size = patch_size,
-            batch_size = batch_size,
-            device = device
-        )
-
-        print(f"-Training the concept decomposition", end="\r")
-        concept_dataset = torch.Tensor([])
-        for X, y, y_pred in validation_dataloader:
-            print(f"Building concept dataset {len(concept_dataset)}/{concept_dataset_size}", end="\r")
-            if len(concept_dataset) >= concept_dataset_size:
-                break
-            concept_dataset = torch.cat((concept_dataset, X))
-        crops, crops_u, w = craft.fit(concept_dataset)
-        print("-Concept decomposition trained successfully")
-        crops = np.moveaxis(torch_to_numpy(crops), 1, -1)
-        gr = cu.Gradient_retriever(list(model.children())[layer_depth-1])
-
-        results = {}
-        for backprop_step in backprop_steps:
-            print(f"-Gathering concepts with backprop step : {backprop_step}", end="\r")
-            results[backprop_step] = cu.gather_all_concept_results(validation_dataloader, model, loss_fn, {"all":craft}, gr, backprop_mult=backprop_step, device=device)
-            print(f"-Concepts with backprop step {backprop_step} gathered successfully")
-
-            # res = {}
-            # for classe in range(10):
-            #     biases = cu.get_bias_concept(results=results, studied_class=classe, number_of_concepts=number_of_concept)
-            #     study_list = list(biases.items())
-            #     study_list.sort(key=lambda x : x[1])
-            #     for c_id, val in study_list[::-1][:]:
-            #         if c_id in res:
-            #             res[c_id] = max(res[c_id], val)
-            #         else :
-            #             res[c_id] = val
-            # res = [(key, val) for key, val in res.items()]
-            # res.sort(key= lambda x: -x[1])
-
-        # parameters["concept_bias"] = res
-        parameters["concept_results"] = results
-        parameters["concept_parameters"] = {
-            "W" : w,
-            "crops" : crops,
-            "crops_u" : crops_u,
-            "reducer" : craft.reducer,
-        }
-    
-    else:
-        concepts_engines, concept_parameters = cu.train_concept_engines(
-            dataloader=validation_dataloader,
-            model=model,
-            layer_depth=layer_depth,
-            number_of_concept=number_of_concept,
-            concept_decomposer=Craft,
-            patch_size=patch_size,
-            concept_dataset_size=concept_dataset_size,
-            batch_size=batch_size,
-            device=device,
-        )
-        print("-Concept decomposition trained successfully")
-        gr = cu.Gradient_retriever(list(model.children())[layer_depth-1])
-
-        results = {}
-        for backprop_step in backprop_steps:
-            print(f"-Gathering concepts with backprop step : {backprop_step}", end="\r")
-            results[backprop_step] = cu.gather_all_concept_results(validation_dataloader, model, loss_fn, concepts_engines, gr, backprop_mult=backprop_step, device=device, multi_concept=args.multi_concept)
-            print(f"-Concepts with backprop step {backprop_step} gathered successfully")
-
-            # res = {}
-            # for classe in range(10):
-            #     biases = cu.get_bias_concept(results=results, studied_class=classe, number_of_concepts=number_of_concept)
-            #     study_list = list(biases.items())
-            #     study_list.sort(key=lambda x : x[1])
-            #     for c_id, val in study_list[::-1][:]:
-            #         if c_id in res:
-            #             res[c_id] = max(res[c_id], val)
-            #         else :
-            #             res[c_id] = val
-            # res = [(key, val) for key, val in res.items()]
-            # res.sort(key= lambda x: -x[1])
-
-        # parameters["concept_bias"] = res
-        parameters["concept_results"] = results
-        parameters["concept_parameters"] = concept_parameters
-
-    with open(result_path, "wb") as f:
-        pkl.dump(parameters, f)
-    
-    # run.finish()
-    print(f"OOOOOOOOO Concept experiment number {model_id}|{concept_id} runned with sucess OOOOOOOOO")
+out_path = os.path.join(result_path, f"concepts_{model_id}_{concept_id}.pkl")
+with open(out_path, "wb") as f:
+    pkl.dump(parameters, f)
+print(f"OOOOOOOOO Concept experiment number {model_id}|{concept_id} runned with sucess OOOOOOOOO")
 
